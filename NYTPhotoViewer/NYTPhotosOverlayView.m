29c33b50d03a010fd56691bc158e1ad1a40e71bd
Michael Berg <michael.berg@lovoo.com>
2016-09-02 14:05:16 +0200

    Add top gradient to overlayView

--- NYTPhotoViewer/NYTPhotosOverlayView.m
+++ NYTPhotoViewer/NYTPhotosOverlayView.m
@@ -13,6 +13,7 @@
 
 @property (nonatomic) UINavigationItem *navigationItem;
 @property (nonatomic) UINavigationBar *navigationBar;
+@property (nonatomic) CAGradientLayer *topGradientLayer;
 
 @end
 
@@ -54,6 +55,12 @@
     if ([self.captionView conformsToProtocol:@protocol(NYTPhotoCaptionViewLayoutWidthHinting)]) {
         [(id<NYTPhotoCaptionViewLayoutWidthHinting>) self.captionView setPreferredMaxLayoutWidth:self.bounds.size.width];
     }
+    
+    self.topGradientLayer.frame = ({
+        CGRect frame = self.navigationBar.frame;
+        frame.size.height *= 1.5;
+        frame;
+    });
 }
 
 #pragma mark - NYTPhotosOverlayView
@@ -78,6 +85,10 @@
     NSLayoutConstraint *widthConstraint = [NSLayoutConstraint constraintWithItem:self.navigationBar attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeWidth multiplier:1.0 constant:0.0];
     NSLayoutConstraint *horizontalPositionConstraint = [NSLayoutConstraint constraintWithItem:self.navigationBar attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeCenterX multiplier:1.0 constant:0.0];
     [self addConstraints:@[topConstraint, widthConstraint, horizontalPositionConstraint]];
+    
+    self.topGradientLayer = [CAGradientLayer layer];
+    self.topGradientLayer.colors = [NSArray arrayWithObjects:(id)[[UIColor blackColor] colorWithAlphaComponent:0.4].CGColor, (id)[UIColor clearColor].CGColor, nil];
+    [self.layer insertSublayer:self.topGradientLayer atIndex:0];
 }
 
 - (void)setCaptionView:(UIView *)captionView {
